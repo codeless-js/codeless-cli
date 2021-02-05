b8e2c03433d795e631c5222df0e5d1faa9f4c98c
tyuqing <tyuqing7@163.com>
2021-02-05 17:45:00 +0800

    refactor(build-plugin-vue-iview): 加上iview组件库

--- packages/build-plugin-vue-iview/src/template/block.entry.js
+++ packages/build-plugin-vue-iview/src/template/block.entry.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
 import Block from '@/block';
+
+Vue.use(ViewUI);
 
 new Vue({
   render: h => h(Block),
